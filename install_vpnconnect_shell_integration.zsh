686f54eb4952bf066dc7784ee4f15e3fc630e043
Gijs Keij <gijs.keij@bit-key.nl>
2023-07-16 19:04:38 +0200

    installation script

---
+++ install_vpnconnect_shell_integration.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Set the URL of the repository
+REPO_URL="https://github.com/gmkey/tunnelblick_cli_macos"
+
+# Set the directory to install the scripts
+INSTALL_DIR="$HOME/vpnconnect"
+
+# Check if Tunnelblick is installed
+if [ ! -d "/Applications/Tunnelblick.app" ]; then
+    echo "Tunnelblick is not installed. Please download and install it from https://tunnelblick.net/downloads.html"
+    exit 1
+fi
+
+# Create the installation directory if it doesn't exist
+mkdir -p "$INSTALL_DIR"
+
+# Download the scripts
+curl -L "$REPO_URL/raw/main/vpnconnect.sh" -o "$INSTALL_DIR/vpnconnect.sh"
+curl -L "$REPO_URL/raw/main/.vpnconnect_shell_integration.zsh" -o "$HOME/.vpnconnect_shell_integration.zsh"
+
+# Set the permissions on the scripts
+chmod +x "$INSTALL_DIR/vpnconnect.sh"
+
+# Source the shell integration from .zshrc if it's not already there
+if ! grep -q "source $HOME/.vpnconnect_shell_integration.zsh" "$HOME/.zshrc"; then
+    echo "source $HOME/.vpnconnect_shell_integration.zsh" >>"$HOME/.zshrc"
+fi
+
+# Inform the user that the installation is complete
+echo "Installation complete. Please restart your terminal, or run 'source ~/.zshrc'"
